e90d4adb506f917b6adab58f983ef58e0eb3a013
RoselaMPG <rslmpgllrd@gmail.com>
2025-06-07 16:21:40 -0700

    Bridge embedded device and web app

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from mainapp.decorators import role_required
+from core.mqtt_client import latest_message
 
 def home(request):
     return render(request, 'main/index.html')
@@ -15,3 +16,7 @@ def vendor(request):
 @role_required('attendee')
 def attendee(request):
     return render(request, 'main/attendee.html')
+
+def ticket_dashboard(request):
+    msg = latest_message.get("easyconnect/ticket", "No ticket data")
+    return render(request, "mainapp/dashboard.html", {"ticket_info": msg})
